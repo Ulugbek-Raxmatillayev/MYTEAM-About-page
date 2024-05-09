f57281e792ded41edeba2e2883dde1ea98987fa0
Ulugbek-Raxmatillayev <ulugbekraxmatillayev615@gmail.com>
2024-05-09 12:43:37 -0700

    Homework About Page

---
+++ js/script.js
@@ -0,0 +1,90 @@
+let icon_plus1 = document.getElementById('plus1');
+let card_item = document.querySelector('.descript1');
+
+icon_plus1.addEventListener('click', () => {
+    icon_plus1.classList.toggle("plus_x");
+    card_item.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "-9.55rem";
+    card_item2.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item3.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item4.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item5.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item6.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+});
+
+
+
+
+let icon_plus2 = document.getElementById('plus2');
+let card_item2 = document.querySelector('.descript2');
+
+icon_plus2.addEventListener('click', () => {
+    icon_plus2.classList.toggle("plus_x");
+    card_item.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item2.style.top = (card_item2.style.top === "-9.55rem") ? "6rem" : "-9.55rem";
+    card_item3.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item4.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item5.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item6.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+});
+
+
+
+
+let icon_plus3 = document.getElementById('plus3');
+let card_item3 = document.querySelector('.descript3');
+
+icon_plus3.addEventListener('click', () => {
+    icon_plus3.classList.toggle("plus_x");
+    card_item.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item2.style.top = (card_item2.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item3.style.top = (card_item3.style.top === "-9.55rem") ? "6rem" : "-9.55rem";
+    card_item4.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item5.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item6.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+});
+
+
+
+let icon_plus4 = document.getElementById('plus4');
+let card_item4 = document.querySelector('.descript4');
+
+icon_plus4.addEventListener('click', () => {
+    icon_plus4.classList.toggle("plus_x");
+    card_item.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item2.style.top = (card_item2.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item3.style.top = (card_item3.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item4.style.top = (card_item4.style.top === "-9.55rem") ? "6rem" : "-9.55rem";
+    card_item5.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item6.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";    
+});
+
+
+
+
+let icon_plus5 = document.getElementById('plus5');
+let card_item5 = document.querySelector('.descript5');
+
+icon_plus5.addEventListener('click', () => {
+    icon_plus5.classList.toggle("plus_x");
+    card_item.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item2.style.top = (card_item2.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item3.style.top = (card_item3.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item4.style.top = (card_item4.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item5.style.top = (card_item5.style.top === "-9.55rem") ? "6rem" : "-9.55rem";
+    card_item6.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";   
+});
+
+
+
+let icon_plus6 = document.getElementById('plus6');
+let card_item6 = document.querySelector('.descript6');
+
+icon_plus6.addEventListener('click', () => {
+    icon_plus6.classList.toggle("plus_x")
+    card_item.style.top = (card_item.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item2.style.top = (card_item2.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item3.style.top = (card_item3.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item4.style.top = (card_item4.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item5.style.top = (card_item5.style.top === "-9.55rem") ? "6rem" : "6rem";
+    card_item6.style.top = (card_item6.style.top === "-9.55rem") ? "6rem" : "-9.55rem";
+});
